127258e3b527530162ea41ab5cb3aa55c1143056
MalekHassan <malekdalalha1995@gmail.com>
2020-09-06 23:40:24 +0300

    finishing lab-11

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const express = require("express");
 const { request, response } = require("express");
 const cors = require("cors");
 const superagent = require("superagent");
+const { compile } = require("ejs");
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.static('./public'));
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded());
 // app.set('views', './views/pages');
 app.set('view engine','ejs');
 
-app.get('/Hello',(req,res)=>{
+app.get('/',(req,res)=>{
     // res.status(200).send('home page');
     //redirect .. render
     res.render('pages/index');
@@ -22,47 +23,65 @@ app.get('/searches/new',(req,res)=>{
     // console.log(req.query);
     res.render('pages/searches/new');
 })
- app.get('/bookList',getData)
- function getData(req,res){
-     let bookArray=[];
+ app.post('/searches/show',getData)
+  function getData(req,res){
+    //  let bookArray=[];
     // console.log(req.query);
-     let bookName=req.query.stringType;
+     let bookName=req.body.stringType;
      console.log('hi name',bookName)
-     let bookType=req.query.searchType;
+     let bookType=req.body.searchType;
      console.log('hi type',bookType)
+    //  let check = checking(bookName,bookType)
      if (bookType === 'author'){
-   let url=`https:www.googleapis.com/books/v1/volumes?q=${bookName}+inauthor`;
-   console.log('URL: ',url);
+   let url=`https://www.googleapis.com/books/v1/volumes?q=${bookName}+inauthor`;
+//    console.log('URL: ',url);
+//    console.log('superagent', await superagent.get(url));
      superagent.get(url).then (item=>{
-        console.log(item);
-         item.bady.map(element => {
-            let data = new Book (element);
-            bookArray.push(data)
+        // console.log(item);
+        let bookdata = item.body.items.map(element => {
+            return  new Book (element);
+            // console.log('data',data)
+            // bookArray.push(data)
         });
     //    console.log(item.body);
-       res.send(bookArray);
-   });
+       res.render('pages/searches/show',{data:bookdata});
+   })
+   .catch(()=>{
+    errorHandler('something went wrong in getting the data',req,res)
+})
+
      }else{
-        let url=`https:www.googleapis.com/books/v1/volumes?q=${bookName}+intitle`;
-        console.log('URL title :  ',url);
+        let url=`https://www.googleapis.com/books/v1/volumes?q=${bookName}+intitle`;
+        // console.log('URL title :  ',url);
+        // console.log('superagent', await superagent.get(url));
           superagent.get(url).then (item=>{
-            console.log(item);
+            // console.log(item);
             // console.log(item.body);
-            item.bady.map(element => {
-                let data = new Book (element);
-                bookArray.push(data)
-            });            
-            
-     });
-     res.send('hi title')
- }
+            let bookdata = item.body.items.map(element => {
+                return  new Book (element);
+                // console.log('data',data)
+                // bookArray.push(data)
+            });
+           console.log(bookdata);
+           res.render('pages/searches/show',{data:bookdata});
+     })     
+     .catch(()=>{
+        errorHandler('something went wrong in getting the data',req,res)
+    })
+}
 };
 
 function Book(element){
-    this.title=element.title,
-    this.author=element.author,
-    this.descreption=element.descreption
+    this.image = element.volumeInfo.imageLinks.thumbnail || "https://i.imgur.com/J5LVHEL.jpg"
+    this.title=element.volumeInfo.title 
+    this.author=element.volumeInfo.authors || "There is no authors"
+    this.description=element.volumeInfo.description || "There is no description"
+    this.date=element.volumeInfo.publishedDate || "There is no description"
+
 }
+function errorHandler (error, req, res) {
+    res.status(500).send(error);
+};
 
 
 app.listen(PORT,() =>{
