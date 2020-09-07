ceab522476df8b9d83aa9318c9ad3d3924a0943b
Aya-Akrabawi <aya.akrabawi.93@hotmail.com>
2020-09-07 18:25:27 +0300

    lab12 requirments

--- server.js
+++ server.js
@@ -7,17 +7,31 @@ const superagent = require("superagent");
 const { compile } = require("ejs");
 const PORT = process.env.PORT || 8080;
 const app = express();
+const pg = require("pg");
+const client = new pg.Client(process.env.DATABASE_URL);
+
+
 app.use(cors());
 app.use(express.static('./public'));
 app.use(express.urlencoded({extended:true}));
 // app.set('views', './views/pages');
 app.set('view engine','ejs');
 
-app.get('/',(req,res)=>{
-    // res.status(200).send('home page');
-    //redirect .. render
-    res.render('pages/index');
-})
+app.get('/', bookSet);
+
+
+async function bookSet(req,res){
+    let dataBase = await getBookData()
+    res.render('pages/index', { banana:dataBase});
+}
+function getBookData(){
+    let SQL = `SELECT * FROM books`;
+   return client.query(SQL)
+    .then(results =>{
+    return results.rows;
+    })
+    .catch (error => errorHandler(error,req,res))
+}
 
 app.get('/searches/new',(req,res)=>{
     // console.log(req.query);
@@ -39,12 +53,18 @@ app.get('/searches/new',(req,res)=>{
 //    console.log('URL: ',url);
 //    console.log('superagent', await superagent.get(url));
      superagent.get(url).then (item=>{
-        // console.log(item);
+        // console.log(item.body.);
+        // let counter = 0;
         let bookdata = item.body.items.map(element => {
+            //  counter++;
             return  new Book (element);
             // console.log('data',data)
             // bookArray.push(data)
+            
         });
+              
+
+        saveToDataBase(bookdata);
     //    console.log(item.body);
        res.render('pages/searches/show',{data:bookdata});
    })
@@ -52,7 +72,32 @@ app.get('/searches/new',(req,res)=>{
     errorHandler('something went wrong in getting the data',req,res)
 })
   }
+  function saveToDataBase(bookdata){
+      let SQL = `INSERT INTO books ( image_url , title, author, isbn , description) VALUES ($1,$2,$3,$4,$5);`;
+      bookdata.map(item =>{
+        let safeValue = [ item.image , item.title, item.author, item.isbn , item.description];
+        console.log(item.image );
+        client.query(SQL,safeValue).then(()=>{
+          // value.redirect('/');
+            console.log('we are in data base')
+        })
+      })
+     
+  }
+  app.get('/books/:id',viewDetailsBook);
 
+  function viewDetailsBook(req,res) {
+    let SQL = `SELECT * FROM books WHERE id=$1`;
+    // console.log(req.params);
+    let book_id = req.params.id;
+    let values = [book_id];
+    client.query(SQL,values)
+    .then(results=>{
+      // console.log(results.rows);
+      res.render('pages/books/details',{detailedBook: results.rows[0]});
+  
+    })
+  }
 function Book(element){
     this.image = element.volumeInfo.imageLinks.thumbnail || "https://i.imgur.com/J5LVHEL.jpg"
     this.title=element.volumeInfo.title 
@@ -61,14 +106,16 @@ function Book(element){
     element.volumeInfo.industryIdentifiers[0].identifier) ||
         "There is no isbn "
     this.description=element.volumeInfo.description || "There is no description"
-    this.date=element.volumeInfo.publishedDate || "There is no description"
+    // this.date=element.volumeInfo.publishedDate || "There is no description"
 
 }
 function errorHandler (error, req, res) {
     res.status(500).send(error);
 };
 
+client.connect(()=>{
 
 app.listen(PORT,() =>{
     console.log(`You are listening for ${PORT}`);
+});
 });
